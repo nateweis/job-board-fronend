f53b686a28eb6fb254b532b63308c65ed9de8b45
Naftali Weiss <Naftali@PROPUMP.local>
2019-09-12 14:15:08 -0600

    inital booster job data now displays on the page

--- jbf/src/components/BoosterHome.js
+++ jbf/src/components/BoosterHome.js
@@ -14,7 +14,7 @@ class BoosterHome extends Component{
             res.json()
             .then((data) => {
                console.log(data);
-                
+                this.setState({jobs:data.data})
             },(err) => {
                 console.log(err);
                 
@@ -28,6 +28,17 @@ class BoosterHome extends Component{
         return(
             <div>
             <h2>Booster Jobs Index Page</h2>
+            {this.state.jobs? this.state.jobs.map((job, index) => {
+                return(
+                    <div key={index}>
+                        <p>Job Order Number: {job.job_order_number}</p>
+                        <p>Requested By: {job.requested_by} </p>
+                        <p>Site Location: {job.job_address} </p>
+                        <p>Job Description: {job.description} </p>
+                        <p>=====================================</p>
+                    </div>
+                )
+            }) : <h4>Loading.....</h4> }
             </div>
         )
     }
