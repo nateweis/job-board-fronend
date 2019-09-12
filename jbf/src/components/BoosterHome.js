13e179797e9008f9ad209df172bf529e0d50b461
Naftali Weiss <Naftali@PROPUMP.local>
2019-09-12 13:19:39 -0600

    booster page now pulls the booster job data from db

--- jbf/src/components/BoosterHome.js
+++ jbf/src/components/BoosterHome.js
@@ -1,7 +1,28 @@
 import React, {Component} from 'react';
 
 class BoosterHome extends Component{
+    constructor(props) {
+       super(props)
+       this.state = {
 
+       } 
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3000/boosters',{method:"GET"})
+        .then((res) => {
+            res.json()
+            .then((data) => {
+               console.log(data);
+                
+            },(err) => {
+                console.log(err);
+                
+            }
+            
+            )
+        })
+    }
 
     render(){
         return(
