85ac2a6acf6ca53d6821ebd06bd87916e208d238
Naftali Weiss <Naftali@PROPUMP.local>
2019-09-19 10:16:39 -0400

    made a update fetch to the api

--- jbf/src/components/BoosterShow.js
+++ jbf/src/components/BoosterShow.js
@@ -60,7 +60,24 @@ class BoosterShow extends Component{
 
     handleSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state);
+        fetch('http://localhost:3000/boosters',{
+            method: 'PUT',
+            body: JSON.stringify(this.state),
+            headers:{
+                'Accept': 'application/json',
+               'Content-Type': 'application/json',
+               'Authorization' : `Token ${Auth.getToken()}`
+             }
+        })
+        .then((res) => {
+            res.json()
+            .then((data) => {
+                console.log(data)
+            },(err) => {
+                console.log(err);
+                
+            })
+        })
         
     }  
 
