c3c8a720b1c8ca0e61bb6d6af622d9b0436f2239
Naftali Weiss <Naftali@PROPUMP.local>
2019-10-17 13:32:26 -0400

    changed the archive btn a bit and added to the btn drop down

--- jbf/src/components/NavBar.js
+++ jbf/src/components/NavBar.js
@@ -12,6 +12,10 @@ class NavBar extends Component{
         }
     }
 
+    componentDidMount(){
+        document.querySelector(".nav-links-hidden").style.display = "none";
+    }
+
     changeView = (e) => {
         if(e.target.value === "Completed Jobs"){
             this.props.switchToArchive(true)
@@ -32,6 +36,20 @@ class NavBar extends Component{
         this.setState({tab:str})        
     }
 
+    showLinks = () => {
+        const links = document.querySelector(".nav-links-hidden")
+        const spacer = document.querySelector(".spacer")
+
+        if(links.style.display === "none"){
+            links.style.display = "flex";
+            spacer.style.height = "190px";
+        }else{
+            links.style.display = "none";
+            spacer.style.height = "75px";
+        }
+               
+    }
+
     render(){ 
         // let width = window.innerWidth      
         return(
@@ -43,9 +61,15 @@ class NavBar extends Component{
                     
                     <button className="loggout-btn" onClick={this.loggout}>Loggout</button>
 
-                    <div className="nav-tab">
-                        <button value="Pending................" onClick={this.changeView} >Active</button>
-                        <button value="Completed Jobs" onClick={this.changeView} >Completed</button>
+                    <div>
+                        {
+                            this.state.viewState === "Pending................"?
+                            <button value="Completed Jobs" onClick={this.changeView} >Toggle To Archives</button>
+                            :
+                            <button value="Pending................" onClick={this.changeView} >Toggle To Pending Jobs</button>
+                        }
+                        
+                        
                     </div>
 
                     <h1>{this.state.viewState}</h1>
@@ -59,13 +83,33 @@ class NavBar extends Component{
                     </ul>                
                                 
                     </div>
-                    :<button>Stuff</button>}</div>}
-                    
+                    :
+                    <div className="nav-container">
+                        <button className="loggout-btn" onClick={this.loggout}>Loggout</button>
+                        <h1>{this.state.viewState}</h1>
+                        <button onClick={this.showLinks}><i class="fas fa-bars"></i></button>
+                    </div>
+                    }</div>}
+
                 </ReactResizeDetector>
 
+                <div className="spacer"></div>
+
+                <ul className="nav-links-hidden" >
+                    <li>
+                        {
+                            this.state.viewState === "Pending................"?
+                            <button value="Completed Jobs" onClick={this.changeView} >Toggle To Archives</button>
+                            :
+                            <button value="Pending................" onClick={this.changeView} >Toggle To Pending Jobs</button>
+                        }                 
+                    </li>
+                    <li><Link to="/jobs/booster/index"><button onClick={()=> this.saveTab("booster")} >Booster Jobs</button></Link></li> 
+                    <li><Link to="/jobs/sewer/index"><button onClick={()=> this.saveTab("sewer")}>Sewer Jobs</button></Link></li>
+                    <li><Link to="/jobs/fire/index"><button onClick={()=> this.saveTab("fire")}>Fire Jobs</button></Link></li>                   
+                </ul>
             
 
-            <div className="spacer"></div>
             </div>
         )
     }
