bb9d1fbffedf53ce2377d2b80c550763febf19dc
Naftali Weiss <Naftali@PROPUMP.local>
2019-12-09 11:50:58 -0500

    password edit page diffrent for admins

--- src/components/ChangePassword.js
+++ src/components/ChangePassword.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
 class ChangePassword extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            user: this.props.passdownUser
+        }
+    }
 
+    componentDidUpdate(){
+        if(this.state.user.name !== this.props.passdownUser.name){
+            this.setState({user:this.props.passdownUser})
+        }
+    }
 
     render(){
         return(
             <div>
-                <div className="banner">
-                    <h2>The Change Password Page</h2>
-                </div>
-                
+                {this.state.user.admin?
+                    <div className="banner">
+                        <h2>Users Edit Page</h2>
+                    </div>
+                    :
+                    <div className="banner">
+                        <h2>Change Password Page</h2>
+                        <form className="form-style">
+                            <span>
+                                <label htmlFor="">Current Password:  </label>
+                                <input type="text"/>
+                            </span>
+                        </form>
+                    </div>
+                }                               
             </div>
         )
     }
