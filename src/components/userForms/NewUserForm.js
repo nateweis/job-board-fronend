4573bf97b9fb1be5317edb22a160ed6f93823aa8
Naftali Weiss <Naftali@PROPUMP.local>
2019-12-12 11:21:13 -0500

    wired the new user form to the state

--- src/components/userForms/NewUserForm.js
+++ src/components/userForms/NewUserForm.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import Auth from '../../modules/Auth'
 
 class NewUserForm extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            username: "",
+            password: "",
+            name: "",
+            admin: false
+        }
+    }
+
+    handleChange = (e) => {     
+        let val;
+
+        if(e.target.name === "admin"){
+           val = e.target.checked; 
+        }    
+        else{
+            val = e.target.value
+        }     
+        this.setState({[e.target.name]: val});
+    }
+
 
     render(){
         return(
@@ -10,19 +32,23 @@ class NewUserForm extends Component{
                 <h3>Add a New User</h3>
 
                 <span>
-                    <input type="text" name="" />
+                    <label htmlFor="">Username:  </label>
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
                 </span>
                 
                 <span>
-                    <input type="password" name=""/>
+                    <label htmlFor="">Password:   </label>
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
                 </span>
 
                 <span>
-                    <input type="text"/>
+                    <label htmlFor="">Name of User:  </label>
+                    <input type="text"  name="name" val={this.state.name} onChange={this.handleChange} />
                 </span>
 
                 <span>
-                    <input type="checkbox" name="" />
+                    <label htmlFor="">Admin Privlages:   </label>
+                    <input type="checkbox" name="admin" checked={this.state.admin? true: false} onChange={this.handleChange} />
                 </span>
                 
                 <input type="submit" value="Submit"/>
