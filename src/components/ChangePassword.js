4c3055ab11df5fff5ea2be3090e5eca81cf22984
Naftali Weiss <Naftali@PROPUMP.local>
2019-12-16 13:03:04 -0500

    changed the api url for docker

--- src/components/ChangePassword.js
+++ src/components/ChangePassword.js
@@ -52,7 +52,7 @@ class ChangePassword extends Component{
     }
 
     getAllUsers = () => {
-        fetch('https://job-board-api.herokuapp.com/users',{
+        fetch('http://localhost:3001/users',{
             method:"GET",
             headers:{
                 Authorization : `Token ${Auth.getToken()}`
