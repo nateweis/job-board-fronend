33eb10552739a9a2e19244856f6d1747995ee481
Naftali Weiss <Naftali@PROPUMP.local>
2021-01-26 11:32:34 -0500

    added to the stock page

--- src/components/InStock.js
+++ src/components/InStock.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class InStock extends Component{
+    constructor(props){
+        super(props)
+        this.state= {
+            
+        }
+    }
 
     render(){
+
         return(
             <>
-                <h3>The Stock Page</h3>
+                <div className="banner">
+                    <h2>Stock</h2>
+                </div>
+                
+                <div className="flexbox">
+                    <input type="text" placeholder="Add Item" name="" id=""/>
+                    <button>Add</button>
+                </div>
+
+                <div className="show-display">
+                    <ul>
+                        <li>Example <button>Edit</button>  <button>Delete</button></li>
+                        <li>Example <button>Edit</button>  <button>Delete</button></li>
+                        <li>Example <button>Edit</button>  <button>Delete</button></li>
+                        <li>Example <button>Edit</button>  <button>Delete</button></li>
+                        <li>Example <button>Edit</button>  <button>Delete</button></li>
+                        <li>Example <button>Edit</button>  <button>Delete</button></li>
+                    </ul>
+                </div>
             </>
         )
     }
